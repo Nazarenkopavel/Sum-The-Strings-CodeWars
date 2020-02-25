4753199791d13af4a82ca719a88319bf4d616975
Nazarenkopavel <50582798+Nazarenkopavel@users.noreply.github.com>
2020-02-25 23:23:42 +0200

    Create sumThestrings_codewars.js

---
+++ sumThestrings_codewars.js
@@ -0,0 +1,15 @@
+/*
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+  sumStr("4", "5")    // should output "9"
+  sumStr("34", "5")   // should output "39"
+If either input is an empty string, consider it as zero.
+*/
+
+function sumStr(a, b) {
+  return (+a + +b).toString();
+}
+
+console.log(sumStr("1", "5"));
+
+// sumStr = (a,b) => String(+a + +b);  один из вариантов решения
